fe4c911d741fafa38ec9db94e3bd5e2e92b6fc61
kosuke55 <kosuke.tnp@gmail.com>
2023-11-24 20:20:42 +0900

    fixup! refactor(behavior_path_planner): visualize debug information of safety check against dynamic obstacles (#4892)

--- planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
+++ planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
@@ -1954,7 +1954,6 @@ bool AvoidanceModule::isSafePath(
         return false;
       }
     }
-    marker_utils::updateCollisionCheckDebugMap(debug.collision_check, current_debug_data, true);
   }
 
   safe_count_++;
